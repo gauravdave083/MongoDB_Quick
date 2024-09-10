5fea505b6616f8d7fd302d92bd04eb9cfdc77a97
gauravdave083 <gd836398@gmail.com>
2024-09-11 01:25:17 +0530

    updates

--- 03 read.js
+++ 03 read.js
@@ -1,3 +1,5 @@
 db.inventory.find() // Fetch all docs
-
+db.inventory.find({})
 db.inventory.find({qty:90}) // To fetch with attributes
+
+db.inventory.findOne
---
+++ 04 update.js
@@ -0,0 +1,8 @@
+db.inventory.updateOne(
+    {item: "paper"},
+    {
+        $set: { "size.uom": "cm", status: "P"},
+        $currentDate: { lastModified: true }
+    }
+)
+
